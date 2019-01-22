83f9037ed490e81d8396a5756666cc0d95bed67c
pragathibendadi <43053009+pragathibendadi@users.noreply.github.com>
2019-01-21 18:33:52 -0600

    Add files via upload

---
+++ src/main/java/org/launchcode/bookstore/controller/maincontroller.java
@@ -0,0 +1,15 @@
+package org.launchcode.bookstore.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class maincontroller {
+    @RequestMapping("/")
+    public String root(){
+        return "header";
+    }
+
+}
+
